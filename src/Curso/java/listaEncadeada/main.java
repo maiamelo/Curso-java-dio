13968780aa3f355c3eed692451fbf82ea69d7a75
maiamelo <leo-satt@hotmail.com>
2023-03-13 03:53:02 -0300

    att lista

---
+++ src/Curso/java/listaEncadeada/main.java
@@ -0,0 +1,18 @@
+package Curso.java.listaEncadeada;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		ListaEncadeada<String> minhaListaEncadeada = new ListaEncadeada<>();
+		
+		minhaListaEncadeada.add("test 1");
+		
+		System.out.println(minhaListaEncadeada.get(0));
+		
+
+	}
+
+}
